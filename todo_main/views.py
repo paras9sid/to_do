179d2709bd6d22623c144325450beb19c9395e83
Sidharth Jain <sidharthjainp9@gmail.com>
2024-03-01 12:39:51 +0530

    models created for app

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from todo_app.models import Task
 
 def home(request):
-    return render(request,'home.html')
+    tasks = Task.objects.filter(is_completed=False)
+    
+    #check tak model in terminal
+    # print(tasks)
+    
+    context = {
+        'tasks':tasks
+    }
+    return render(request,'home.html',context)
