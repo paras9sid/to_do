296c178d622d431a91514d95d4a608e7ea658f42
Sidharth Jain <sidharthjainp9@gmail.com>
2024-03-04 15:00:20 +0530

    mark_as_undone functionality plemented-1

--- todo_app/views.py
+++ todo_app/views.py
@@ -17,4 +17,11 @@ def mark_as_done(request,pk):
     task.is_completed = True
     task.save()
     return redirect('home')
-    
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    # print(task)
+    # return HttpResponse(task)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -4,13 +4,17 @@ from todo_app.models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
     
-    #check tak model in terminal
+    #check task model in terminal
     # print(tasks)
     
     completed_tasks = Task.objects.filter(is_completed=True)
     # print(completed_tasks)
+    
+    uncompleted_tasks = Task.objects.filter(is_completed=False)
+    # print(uncompleted_tasks)
     context = {
         'tasks':tasks,
         'completed_tasks':completed_tasks,
+        'uncompleted_tasks':uncompleted_tasks,
     }
     return render(request,'home.html',context)
